ad96c63a9e832991ec67cfa13e0477c30298fc47
Gnanasekaran Perumal <gnanasekaranpg@gmail.com>
2020-05-03 08:51:10 +0530

    pdfviewer resize updated

--- src/ios/PDFViewer.m
+++ src/ios/PDFViewer.m
@@ -4,7 +4,8 @@
 #import <PDFKit/PDFKit.h>
 #import <WebKit/WebKit.h>
 
-@interface PDFViewer : CDVPlugin <PDFViewDelegate>
+API_AVAILABLE(ios(11.0))
+@interface PDFViewer : CDVPlugin
 @property(nonatomic, strong) CDVInvokedUrlCommand* command;
 @property(nonatomic, strong) CDVPluginResult* pluginResult;
 @property(nonatomic, strong) PDFView* pdfView;
@@ -25,28 +26,18 @@
         @try {
             NSString* base64Str = [command.arguments objectAtIndex:0];
             NSString* pageNoStr = [command.arguments objectAtIndex:1];
-            NSString* fitToPage = [command.arguments objectAtIndex:2];
-            NSString* mode = [command.arguments objectAtIndex:3];
-            NSString* autoResize = [command.arguments objectAtIndex:4];
-            NSString* direction = [command.arguments objectAtIndex:5];
-            NSString* fileName = [command.arguments objectAtIndex:6];
+            NSString* mode = [command.arguments objectAtIndex:2];
+            NSString* direction = [command.arguments objectAtIndex:3];
+            NSString* fileName = [command.arguments objectAtIndex:4];
             
-            self.pdfView = [[PDFView alloc] initWithFrame:CGRectMake(0, 55, self.webView.bounds.size.width, self.webView.bounds.size.height)];
+            self.pdfView = [[PDFView alloc] initWithFrame:CGRectMake(0, 70, self.webView.bounds.size.width, self.webView.bounds.size.height)];
             self.pdfView.displayMode = [mode integerValue];
             self.pdfView.displayDirection = [direction  isEqual: @"vertical"] ? kPDFDisplayDirectionVertical : kPDFDisplayDirectionHorizontal;
             self.pdfView.minScaleFactor = self.pdfView.scaleFactor;
             self.pdfView.maxScaleFactor = self.pdfView.scaleFactorForSizeToFit;
             self.pdfView.backgroundColor = [UIColor colorWithRed:243/255.0 green:241/255.0 blue:238/255.0 alpha:1.0];
-            
-            if(![fitToPage isEqual:[NSNull null]]) {
-                [self.pdfView sizeToFit];
-            }
-            
-            if(autoResize) {
-                self.pdfView.autoScales = true;
-            }
-            
-            self.pdfView.delegate = self;
+            self.pdfView.autoScales = true;
+            [self.pdfView sizeToFit];
           
             NSData* data = [[NSData alloc] initWithBase64EncodedString:base64Str options: NSDataBase64DecodingIgnoreUnknownCharacters];
             PDFDocument* pdfDocument = [[PDFDocument alloc] initWithData:data];
@@ -66,15 +57,15 @@
             [self.webView addSubview:self.pdfView];
             [self.webView bringSubviewToFront: self.pdfView];
         
-            self.toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0,        self.webView.bounds.size.width, 50)];
-            self.toolbar.barTintColor =[UIColor colorWithRed:243/255.0 green:241/255.0 blue:238/255.0 alpha:1.0];
+            self.toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 20,        self.webView.bounds.size.width, 90)];
+            self.toolbar.barTintColor = [UIColor colorWithRed:243/255.0 green:241/255.0 blue:238/255.0 alpha:1.0];
+            self.toolbar.backgroundColor = [UIColor colorWithRed:243/255.0 green:241/255.0 blue:238/255.0 alpha:1.0];
             [self.toolbar sizeToFit];
             [self.webView addSubview:self.toolbar];
             [self.webView bringSubviewToFront: self.toolbar];
-            
               
             // toolbar title
-            self.label = [[UILabel alloc] initWithFrame:CGRectMake((self.webView.bounds.size.width/2) - 100, 15, 50, 40)];
+            self.label = [[UILabel alloc] initWithFrame:CGRectMake((self.webView.bounds.size.width/2) - 150, 30, 50, 40)];
             self.label.text = fileName;
             self.label.font = [UIFont boldSystemFontOfSize:20.0];
             [self.label sizeToFit];
@@ -87,10 +78,14 @@
             [self.button setTitle:@"done" forState:UIControlStateNormal];
             [self.button setTitleColor:[UIColor colorWithRed:0/255.0 green:87/255.0 blue:128/255.0 alpha:1.0] forState:UIControlStateNormal];
             [self.button sizeToFit];
-            self.button.frame = CGRectMake(20, 10, 50, 40.0);
+            self.button.frame = CGRectMake(20, 20, 50, 40.0);
             [self.webView addSubview:self.button];
             [self.webView bringSubviewToFront:self.button];
             
+            self.webView.autoresizingMask = UIViewAutoresizingFlexibleRightMargin |
+            UIViewAutoresizingFlexibleLeftMargin |
+            UIViewAutoresizingFlexibleBottomMargin;
+                        
         } @catch(NSException* exception) {
             msg = exception.reason;
             [self sendPluginResult: msg];
--- www/PDFViewer.js
+++ www/PDFViewer.js
@@ -2,9 +2,7 @@ var exec = require("cordova/exec");
 
 function opts(options) {
   options.page = options.page || null;
-  options.fitToPage = options.fitToPage || null;
   options.mode = options.mode || "1";
-  options.autoResize = options.autoResize || true;
   options.fileName = options.fileName || "";
   options.direction = options.direction || "vertical";
 
@@ -29,9 +27,7 @@ exports.viewPDF = function (data, options) {
     cordova.exec(resolve, reject, "PDFViewer", "viewPDF", [
       data,
       options.page,
-      options.fitToPage,
       options.mode,
-      options.autoResize,
       options.direction,
       options.fileName,
     ]);
